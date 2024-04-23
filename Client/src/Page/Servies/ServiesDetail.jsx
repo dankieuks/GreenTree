a3713b6dcedbea267197c9b976b78f399176875c
dankieu <kieudinhdan02@gmail.com>
2024-04-23 17:04:15 +0700

    contact

--- Client/src/Page/Servies/ServiesDetail.jsx
+++ Client/src/Page/Servies/ServiesDetail.jsx
@@ -1,5 +1,6 @@
 import { AiOutlineTags } from "react-icons/ai";
 import React from "react";
+import Strengthcard from "../../Components/Slider/Strengthcard.jsx";
 
 function ServiesDetail() {
   return (
@@ -8,14 +9,9 @@ function ServiesDetail() {
         <AiOutlineTags />
         <h1> Cắt tỉa đường phố ,..vvv.</h1>
       </span>
-      <article className="grid grid-cols-4">
-        <img
-          src="https://nhatminhlandscape.com/files/images/product/tk-thi-cong-canh-quan/D%E1%BB%B1%20%C3%A1n%20C%C3%B4ng%20vi%C3%AAn%20V%E1%BA%A1n%20Ph%C3%A1t%20S%C3%B4ng%20H%E1%BA%ADu%2C%20t%E1%BB%95ng%20di%E1%BB%87n%20t%C3%ADch%203.200m2.jpg"
-          alt=""
-          className="col-span-2"
-        />
-        <div className="col-span-2 mx-5">
-          <h1 className="font-bold text-4xl text-blue-600 uppercase">
+      <article className="grid grid-cols-4 my-4">
+        <div className=" col-span-4 lg:col-span-2 mx-5">
+          <h1 className="font-bold text-4xl text-blue-600 uppercase mb-4">
             Thiết kế Biệt thự nhà vườn
           </h1>
           <p>
@@ -25,8 +21,15 @@ function ServiesDetail() {
             ty chuyên nhận chăm sóc và bảo dưỡng cây xanh hơn 10 năm qua. Chúng
             tôi tin với tìm lực và kinh nghiệm sẽ luôn làm hài lòng khách hàng.
           </p>
+          <button className="btn1 m-5">Liên hệ : 0348216852</button>
         </div>
+        <img
+          src="https://nhatminhlandscape.com/files/images/product/tk-thi-cong-canh-quan/D%E1%BB%B1%20%C3%A1n%20C%C3%B4ng%20vi%C3%AAn%20V%E1%BA%A1n%20Ph%C3%A1t%20S%C3%B4ng%20H%E1%BA%ADu%2C%20t%E1%BB%95ng%20di%E1%BB%87n%20t%C3%ADch%203.200m2.jpg"
+          alt=""
+          className=" col-span-4 px-5 lg:col-span-2 "
+        />
       </article>
+      <Strengthcard />
     </section>
   );
 }
